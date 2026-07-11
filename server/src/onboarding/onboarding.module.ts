312cf4da69b8ea52309924d5911a627a3af17f0e
hagernofal <hagernofal8@gmail.com>
2026-07-12 01:59:03 +0300

    feat:upload logo fro organization

--- server/src/onboarding/onboarding.module.ts
+++ server/src/onboarding/onboarding.module.ts
@@ -1,10 +1,14 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { OnboardingService } from './onboarding.service';
 import { OnboardingController } from './onboarding.controller';
 import { PrismaModule } from '../prisma/prisma.module';
-
+import { UploadModule } from '../upload/upload.module';
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    UploadModule,
+  ],
   controllers: [OnboardingController],
   providers: [OnboardingService],
 })
